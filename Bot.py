6128dc6db49cc474b1bbffa3635e8d5510088736
ridwanasad32-arch <ridwanasad32@gmail.com>
2026-03-22 11:07:47 +0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -342,7 +342,7 @@ def callback(call):
             caption="📷 STRUK ORDER\n================\nID: " + tid + "\nAkun: #" + str(akun_id) + " " + str(akun[3]) + "\nHarga: Rp " + str(akun[6]) + "\n================\nTransfer ke:\nDANA: 085649642594\nGoPay: 085649642594\n================\nScan QRIS di atas untuk pembayaran!\n================\nSetelah transfer ketik:\n/bayar " + tid
         )
         if ADMIN_ID != 0:
-        bot.send_message(ADMIN_ID, "Ada pembeli!\nID: " + tid + "\nPembeli: " + nama + "\nAkun: #" + str(akun_id) + "\nHarga: Rp " + str(akun[6]))
+            bot.send_message(ADMIN_ID, "Ada pembeli!\nID: " + tid + "\nPembeli: " + nama + "\nAkun: #" + str(akun_id) + "\nHarga: Rp " + str(akun[6]))
     elif call.data.startswith("beli_")
         if ADMIN_ID != 0:
             bot.send_message(ADMIN_ID, "Ada pembeli!\nID: " + tid + "\nPembeli: " + nama + "\nAkun: #" + str(akun_id) + "\nHarga: Rp " + str(akun[6]))
